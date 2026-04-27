fc04248e40265c71b772d0e51c1bd9daa8b7230d
Droingo <cmbmx57@gmail.com>
2026-04-27 19:02:37 +1000

    Initial commit

---
+++ src/main/java/net/droingo/aquietplace/AQuietPlaceClient.java
@@ -0,0 +1,10 @@
+package net.droingo.aquietplace;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AQuietPlaceClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
